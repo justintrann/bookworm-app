f17dbf694cd08c428e88bdbe92f85bb3c76af23c
justintrann <justinsang15@gmail.com>
2022-03-14 20:40:50 +0700

    REFACTOR: Correct array

--- app/Http/Resources/BookResource.php
+++ app/Http/Resources/BookResource.php
@@ -20,12 +20,11 @@ class BookResource extends JsonResource
             'book_summary' => $this->book_summary,
             'book_price' => $this->book_price + 0,
             'book_cover_photo' => $this->book_cover_photo,
-            'category_id' => $this->category_id,
-            'author_id' => $this->author_id,
+            'category_info' => $this->category,
+            'author_info' => $this->author,
             'final_price' => $this->final_price + 0,
             'avg_rate' => $this->avg_rate + 0,
             'counted_review' => $this->counted_review + 0,
-            'star_list' => $this->star_list + 0,
         ];
     }
 }
